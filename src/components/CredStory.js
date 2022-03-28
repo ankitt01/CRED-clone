259553b9e6c21f321690cc2d647a95ad7fe37bb1
ankitt01 <ankitrawat0102@gmail.com>
2022-03-29 00:08:33 +0530

    cred story

--- src/components/CredStory.js
+++ src/components/CredStory.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 const CredStory = () => {
   return (
-    <div className='flex'>
-        <div className="the story of CRED begins with trust."></div>
-        <div className="">
+    <div className='h-auto flex justify-center items-center bg-story'>
+      <div className='flex flex-1 flex-col lg:flex-row justify-center items-center w-4/5 lg:w-3/5 m-auto py-20'>
+        <div className="text-7xl lg:text-5xl font-bold p-12 pb-20 lg:pb-0">the story of CRED begins with trust.</div>
+        <div className="text-lg p-12 lg:w-3/5 lg:mr-20 xl:text-2xl ">
           <p>trust as a virtue has consistently played an essential role in every great human achievement. and consistently, its importance has been overlooked. not just by individuals, but by entire societies. we felt it was time someone gave it the spotlight it deserves. especially for the ones who live by this virtue: the trustworthy.</p>
           <p>so we thought of creating a system that rewards its members for doing good and being trustworthy. this way, trust as a virtue becomes something to aspire to, just the way it should be. then we went one step ahead: we built it. we know we are on the right track because here you are.</p>
           <p>if you make it to CRED, congratulations and welcome. we have a lot of things planned for you.</p>
         </div>
+      </div>
     </div>
   )
 }
